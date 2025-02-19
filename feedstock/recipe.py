96662eac047d838f0e679cd65c346660c3bcd42b
Raphael Hagen <norlandrhagen@gmail.com>
2025-02-19 15:04:18 -0700

    wip

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -10,7 +10,7 @@ import os
 import apache_beam as beam
 import numpy as np
 import xarray as xr
-
+import s3fs 
 import obstore as obs
 from obstore.fsspec import AsyncFsspecStore
 from obstore.store import from_url, S3Store
@@ -20,6 +20,7 @@ from pangeo_forge_recipes.transforms import (
     ConsolidateMetadata,
     StoreToZarr,
 )
+from pangeo_forge_recipes.storage import FSSpecTarget
 
 
 input_urls = [
@@ -28,25 +29,15 @@ input_urls = [
 ]
 pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
 
-access_key_id = os.environ['access_key_id']
-secret_access_key = os.environ['secret_access_key']
+# access_key_id = os.environ['access_key_id']
+# secret_access_key = os.environ['secret_access_key']
 read_store = S3Store(
     "leap-pangeo-manual/CASM",
     aws_endpoint="https://nyu1.osn.mghpcc.org",
-    access_key_id=access_key_id,
-    secret_access_key=secret_access_key,
+    access_key_id="",
+    secret_access_key="",
 )
 read_fss = AsyncFsspecStore(read_store)
-
-
-@dataclass
-class RenameDate(beam.PTransform):
-    def _rename_date(self, ds: xr.Dataset) -> xr.Dataset:
-        ds = ds.rename({'date': 'time'})
-        return ds
-
-    def expand(self, pcoll):
-        return pcoll | '_rename_date' >> beam.MapTuple(lambda k, v: (k, self._rename_date(v)))
 
 
 
@@ -56,21 +47,28 @@ class OpenXarrayObsStore(beam.PTransform):
     read_fss: AsyncFsspecStore
     def _obs(self, url: str) -> xr.Dataset:
         _, suffix = url.rsplit('/',1)
-        return xr.open_dataset(self.read_fss.open(suffix), engine='h5netcdf')
+        return xr.open_dataset(self.read_fss.open(suffix), engine='h5netcdf').rename({'date': 'time'})
 
     def expand(self, pcoll):
         return pcoll | 'obs' >> beam.MapTuple(lambda k, v: (k, self._obs(v)))
-    
 
-pattern = pattern.prune()
+
+
+
+fs = s3fs.S3FileSystem(
+       key= "",
+       secret = "",
+       client_kwargs={"endpoint_url":"https://nyu1.osn.mghpcc.org"}
+)
+target_root = FSSpecTarget(fs, 'leap-pangeo-pipeline/CASM/obstore')
+
 with beam.Pipeline() as p:
     (
         p
         | beam.Create(pattern.items())
         | OpenXarrayObsStore(read_fss)
-        | RenameDate()
         | StoreToZarr(
-            target_root='.',
+            target_root=target_root,
             store_name='casm.zarr',
             combine_dims=pattern.combine_dim_keys
         )
@@ -81,16 +79,12 @@ with beam.Pipeline() as p:
 
 # casm = (
 #     beam.Create(pattern.items())
-#     | beam.Create(pattern.items())
-    # | OpenXarrayObsStore(read_fss)
-#     | RenameDate()
+#     | OpenXarrayObsStore(read_fss)
 #     | StoreToZarr(
 #         store_name='casm.zarr',
 #         combine_dims=pattern.combine_dim_keys,
-#         target_chunks={
-#             '#todo!
-#         },
+#         target_chunks={'time':200,'lat':200, 'lon':300},
 #     )
-#     | ConsolidateDimensionCoordinates()
-#     | ConsolidateMetadata()
+#     # | ConsolidateDimensionCoordinates()
+#     # | ConsolidateMetadata()
 # )
